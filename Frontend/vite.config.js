2ed4148c770f59d0e5479457a1012e38a5f232af
Abruzzi96 <tiritoglumert@gmail.com>
2024-12-06 20:57:06 +0300

    fix

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,13 +1,7 @@
-// vite.config.js
-export default {
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
